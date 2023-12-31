391167911f2768492a4f84658b4bb9ac5b7d1f81
RoqayaMohmaad <roqayamohammadrayan@gmail.com>
2023-12-31 13:25:01 +0200

    extensions

---
+++ ChattingApp/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ChattingApp.Data;
+using ChattingApp.Interfaces;
+using ChattingApp.Sevices;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChattingApp.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+    }
+}
---
+++ ChattingApp/Sevices/TokenService.cs
@@ -0,0 +1,43 @@
+﻿using ChattingApp.Interfaces;
+using ChattingApp.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ChattingApp.Sevices
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+                
+        }
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+           {
+               new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+           };
+
+            //SecurityTokenDescriptor is a class in the Microsoft.IdentityModel.Tokens namespace that represents the metadata needed to create a security token, such as a JSON Web Token (JWT).
+            var creds = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokendescriptor = new SecurityTokenDescriptor
+
+            {
+
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokendescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
